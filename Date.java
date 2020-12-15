75d0855a52f05ef54b2775796441d61e773a2173
navkc11 <63931144+navkc11@users.noreply.github.com>
2020-12-15 23:54:32 +0200

    Add files via upload

---
+++ Date.java
@@ -0,0 +1,11 @@
+package com.company;
+//targil 2
+public class Date {
+    int day,month,year;
+    public Date(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+
+    }
+}
---
+++ employee.java
@@ -0,0 +1,23 @@
+package com.company;
+//targil 1 לא ידעתי איך לפתור את התרגילים
+public class employee {
+
+    String name;
+    int salary;
+    double seniority;
+
+    public employee(String name, int salary, double seniority) {
+        this.name = name;
+        this.salary = salary;
+        this.seniority = seniority;
+
+
+    }
+    public String[] method2(){
+        String ratio[];
+        for(int i=0; i<)
+        if(seniority>10 && salary<15000){}
+
+        return ratio;
+    }
+}
