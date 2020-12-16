f219416d98aa3e5ce5f0250d37cfc9b5f4ed3881
navkc11 <63931144+navkc11@users.noreply.github.com>
2020-12-16 17:10:06 +0200

    Update and rename employee.java to Employee.java

---
+++ Employee.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.ArrayList;
+
+//targil 1 לא ידעתי איך לפתור את התרגילים
+public class Employee {
+
+    String name;
+    int salary;
+    int seniority;
+
+    public Employee(String name, int salary, int seniority) {
+        this.name = name;
+        this.salary = salary;
+        this.seniority = seniority;
+
+
+    }
+    //method 1
+    public Double efficency(){
+        return (double)(salary/seniority);
+    }
+    //method 2
+    public ArrayList<Employee> filter(Employee[] allEmployees){
+        ArrayList<Employee> res = new ArrayList<Employee>();
+        for(Employee e : allEmployees){
+            if( e.salary<15000 && e.seniority>120 ) res.add(e);
+        }
+        return res;
+    }
+}
